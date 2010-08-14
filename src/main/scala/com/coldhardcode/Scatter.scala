c5544a534badcd47ec818b8f5818c1637c741213
Cory Watson <github@onemogin.com>
2010-08-13 23:28:37 -0500

    Discover the Root controller.  Does nothing else.

--- src/main/scala/com/coldhardcode/Scatter.scala
+++ src/main/scala/com/coldhardcode/Scatter.scala
@@ -1,40 +1,72 @@
 package com.coldhardcode
 
-import java.io.{PrintWriter, IOException};
-import javax.servlet.{RequestDispatcher, ServletContextListener, ServletContextEvent};
-import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse};
+import java.io.{PrintWriter, IOException}
+import javax.servlet.{RequestDispatcher, ServletContextListener, ServletContextEvent}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
-class Scatter extends HttpServlet { // implements ServletContextListener {
+import com.coldhardcode.scatter.annotation.Public
+import com.coldhardcode.WebClasspathDiscoverer
 
-/*
-    def contextInitialized(ServletContextEvent event) {
+import com.impetus.annovention.{Discoverer,ClasspathDiscoverer}
+import com.impetus.annovention.listener.{MethodAnnotationDiscoveryListener}
 
-        out.println("Starting Up");
+class Scatter extends HttpServlet with ServletContextListener with MethodAnnotationDiscoveryListener {
+
+    def contextInitialized(event:ServletContextEvent) : Unit = {
+
+        println("### Starting Up")
+
+        val disc:Discoverer = new WebClasspathDiscoverer(event.getServletContext);
+        disc.addAnnotationListener(this)
+        println("### Firing Discoverer")
+        disc.discover
+        println("### Done")
     }
 
-    def contextDestroyed(ServletContextEvent event) {
+    def contextDestroyed(event:ServletContextEvent) : Unit = {
 
-        out.println("Shutting Down");
+        println("### Shutting Down")
     }
-*/
+
+    /* Annotation Shit! */
+
+    override def discovered(clas:String, method:String, annotation:String) : Unit = {
+
+        println("Discovered " + clas + "." + method + ": " + annotation);
+    }
+    
+    override def supportedAnnotations() : Array[java.lang.String] = {
+
+        println("Registering interesting Annotations")
+        return Array("com.coldhardcode.scatter.annotation.Public")
+    };
+
+    /* Servlet Shit! */
 
     override def doGet(req:HttpServletRequest, resp:HttpServletResponse) : Unit = {
 
-        val out:PrintWriter = resp.getWriter;
-        out.println("<html>");
-        out.println("<h1>GET</h1");
-        out.println("</html");
-
-        val dispatcher:RequestDispatcher = getServletConfig.getServletContext.getRequestDispatcher("/hello.jsp");
-        dispatcher.forward(req, resp);
+        handleRequest(req, resp)
     }
 
     override def doPost(req:HttpServletRequest, resp:HttpServletResponse) : Unit  = {
 
+        handleRequest(req, resp)
+    }
 
-        val out:PrintWriter = resp.getWriter;
-        out.println("<html>");
-        out.println("<h1>POST</h1");
-        out.println("</html");
+    def handleRequest(req:HttpServletRequest, resp:HttpServletResponse) : Unit = {
+
+        println("Request Information")
+        println("Request URI: " + req.getRequestURI)
+        println("Request URL: " + req.getRequestURL)
+        println("Servlet Path: " + req.getServletPath)
+        println("Context Path: " + req.getContextPath)
+        println("Query String: " + req.getQueryString)
+        println("Path Info: " + req.getPathInfo)
+        println("Path Translated: " + req.getPathTranslated)
+
+        val defaultTemplate:String = req.getPathInfo
+
+        val dispatcher:RequestDispatcher = getServletConfig.getServletContext.getRequestDispatcher(defaultTemplate + ".jsp")
+        dispatcher.forward(req, resp)
     }
 }
---
+++ src/main/scala/com/coldhardcode/myapp/controller/Root.scala
@@ -0,0 +1,12 @@
+package com.coldhardcode.myapp.controller
+
+import com.coldhardcode.scatter.annotation.Public
+
+class Root {
+
+    @Public
+    def default : Unit = {
+
+        println("### default")
+    }
+}
